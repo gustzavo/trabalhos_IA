813fb41947d22a3f5b6f2ef2ab2a0a1c19ee091b
Marcio Rocha <dev.marcio.rocha@gmail.com>
2022-03-30 09:24:01 -0300

    resolved conflicts

--- trabalhos/graça/src/mymain.java
+++ trabalhos/graça/src/mymain.java
@@ -8,10 +8,16 @@ public class mymain {
             {"8","5","X"}});*/
         buscas busca = new buscas();
         //busca.greedy_search();
+<<<<<<< HEAD
         //busca.blind_search_depth(19,true);//n<20 se não demora muito....resulta em: não resolver todos os problemas, mas a maioria!.
         //busca.stAr();
         //busca.aprofundamento_iterativo(20);
         busca.breadth_search();
+=======
+        //busca.blind_search_depth(20);//n<20 se não demora muito....resulta em: não resolver todos os problemas, mas a maioria!.
+        //busca.stAr();
+        busca.IDAstar(20);
+>>>>>>> d0e057b (alterado para testar o novo método IDA*)
     }
 
 
